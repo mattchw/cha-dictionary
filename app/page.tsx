879bbb1d5657e26a69844bc02c2c1fb9b906d2ac
Matthew Wong <matthew.chohin@gmail.com>
2026-09-24 09:22:04 +0100

    Remove leftover `setEntryCopied(false)` call after copy-entry feature removal

--- app/page.tsx
+++ app/page.tsx
@@ -170,7 +170,6 @@ export default function Home() {
     setSuggestOpen(false);
     setActiveIndex(-1);
     setCopied(false);
-    setEntryCopied(false);
     setExpandedMeanings(new Set());
     setExpandingMeaning(null);
     inputRef.current?.focus();
